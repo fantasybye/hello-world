9b4e8fa70d407176404350f067e4cd46107c412a
huomozhihen <yq13yq13@sina.com>
2016-11-19 20:14:36 +0800

    logic、数据库修改

---
+++ src/main/java/data/riskFollowLogic.java
@@ -0,0 +1,9 @@
+package data;
+
+import model.risk;
+
+public class riskFollowLogic {
+//	public risk getLatestRiskInfo(int riskId,int ){
+//		risk result=
+//	}
+}
--- src/main/java/model/project.java
+++ src/main/java/model/project.java
@@ -4,4 +4,23 @@ public class project {
 	private int id;
 	private String projectName;
 	private String creatorName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getCreatorName() {
+		return creatorName;
+	}
+	public void setCreatorName(String creatorName) {
+		this.creatorName = creatorName;
+	}
+
 }
