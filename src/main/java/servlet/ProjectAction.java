e0d2ce7d3afafff5ac6a37381e05a34f454046a3
jlj <jlj@jlj-PC>
2016-11-19 22:27:34 +0800

    projectaction

---
+++ src/main/java/servlet/ProjectAction.java
@@ -0,0 +1,86 @@
+package servlet;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import data.handle;
+import data.projectLogic;
+import data.riskLogic;
+import model.risk;
+
+public class ProjectAction extends ActionSupport{
+	
+	private int selectedprojectid;
+	private ArrayList<risk> risklist;
+	private String plan_name;
+	
+	public int getSelectedprojectid() {
+		return selectedprojectid;
+	}
+
+	public void setSelectedprojectid(int selectedprojectid) {
+		this.selectedprojectid = selectedprojectid;
+	}
+
+	public ArrayList<risk> getRisklist() {
+		return risklist;
+	}
+
+	public void setRisklist(ArrayList<risk> risklist) {
+		this.risklist = risklist;
+	}
+
+	public String getPlan_name() {
+		return plan_name;
+	}
+
+	public void setPlan_name(String plan_name) {
+		this.plan_name = plan_name;
+	}
+
+	
+	
+	public String getProjectRisk(){
+		riskLogic r=new riskLogic();
+		risklist=r.getExistRisk(selectedprojectid);
+		setRiskList();
+		return "success";
+	}
+	
+	private void setRiskList(){
+		for(int i=0;i<risklist.size();i++){
+			switch(risklist.get(i).getRiskPossibility()){
+				case 1:
+					risklist.get(i).setRiskPossibilityStr("低");
+					break;
+				case 2:
+					risklist.get(i).setRiskPossibilityStr("中");
+					break;
+				case 3:
+					risklist.get(i).setRiskPossibilityStr("高");
+					break;
+				default:
+					System.out.println("riskPossibility false");
+					break;
+			}
+			
+			switch(risklist.get(i).getRiskEfficiency()){
+				case 1:
+					risklist.get(i).setRiskEfficiencyStr("低");
+					break;
+				case 2:
+					risklist.get(i).setRiskEfficiencyStr("中");
+					break;
+				case 3:
+					risklist.get(i).setRiskEfficiencyStr("高");
+					break;
+				default:
+					System.out.println("riskEfficiencyStr false");
+					break;
+			}
+		}
+	}
+}
